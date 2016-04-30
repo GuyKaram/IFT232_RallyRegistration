385b74c087bc9eb5c27acf43ef8bf00271c86bad
Guy Karam <>
2016-04-30 21:04:31 +0300

    IFT232 Project

---
+++ src/ift232_rally/IFT232_Rally.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ift232_rally;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class IFT232_Rally {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
